6c431b6c7db96dd61f862172873ec3d83f817561
Javier Corral Lizárraga <javiercorraljr@hotmail.com>
2020-08-05 21:56:54 -0500

    remplazador de blockchains

--- src/blockchain/blockchain.js
+++ src/blockchain/blockchain.js
@@ -1,4 +1,5 @@
 import Block from './block';
+import validate from './modules/validate';
 
 class Blockchain {
     constructor() {
@@ -11,6 +12,20 @@ class Blockchain {
 
             return block;
     }
+
+    replace(newBlocks = []){
+        if(newBlocks.lenght < this.blocks.lenght) throw Error('reveived chain isnt longer than the current chain');
+        try{
+            validate(newBlocks);
+        }catch (error){
+            throw Error('reveived chain is invalid');
+        }
+
+        this.blocks = newBlocks;
+
+        return this.blocks;
+    }
+
 }
 
 export defautl Blockchain;
---
+++ src/blockchain/blockchain.js~
@@ -0,0 +1,16 @@
+import Block from './block';
+
+class Blockchain {
+    constructor() {
+        this.blocks = [Block.genesis];
+    }
+
+    addBlock(data) {
+        const previousBlock = this.blocks[this.blocks-lenght -1];
+        const block = Block.mine(previousBlock, datas. blocks. push(block);
+
+            return block;
+    }
+}
+
+export defautl Blockchain;
